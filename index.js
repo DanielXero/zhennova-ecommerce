c3992a68fb5be881536f788a143317c1ab0abe00
Gustavo Sosa <117545957+DanielXero@users.noreply.github.com>
2025-10-14 09:02:42 -0300

    crear archivo app.js y configurar servidor

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-// index.js
-const http = require('http');
+const app = require('./src/app')
+require('dotenv').config({quiet: true})
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain' );
-  res.end('Hola, Mundo!');
-});
 
-const PORT = 8000;
-server.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+
+const PORT = process.env.PORT || 3001
+
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor Zhennova escuchando en http://localhost:${PORT}`)
+})
