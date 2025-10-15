7eaa64a6cee6cc9a6d883bdd2e01ced436f5a23c
Gustavo Sosa <117545957+DanielXero@users.noreply.github.com>
2025-10-15 06:39:08 -0300

    crear base de datos simulada con usuarios y productos

---
+++ src/db/database.js
@@ -0,0 +1,34 @@
+const users = [
+  { id: 1, name: "Juan Pérez", email: "juan@zhennova.com" },
+  { id: 2, name: "María López", email: "maria@zhennova.com" },
+  { id: 3, name: "Carlos Gómez", email: "carlos@zhennova.com" },
+];
+
+const products = [
+  {
+    id: 1,
+    name: "Teclado Mecánico RGB",
+    price: 85000,
+    category: "Periféricos",
+  },
+  {
+    id: 2,
+    name: "Memoria RAM 16GB DDR4",
+    price: 34000,
+    category: "Componentes",
+  },
+  {
+    id: 3,
+    name: "Fuente de Poder 650W 80+ Bronze",
+    price: 80000,
+    category: "Componentes",
+  },
+  {
+    id: 4,
+    name: 'Monitor 24" 144Hz',
+    price: 150000,
+    category: "Periféricos",
+  },
+];
+
+module.exports = {users, products}
