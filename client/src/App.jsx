c4978c81ae33f2bcc6d13fc0b459d035ae4bb546
Gustavo Sosa <117545957+DanielXero@users.noreply.github.com>
2025-11-23 12:07:29 -0300

    actualizando estructura

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { Header } from './components/Header';
-import { Home } from './components/Home';
+import { Home } from './pages/Home';
 import { Footer } from './components/Footer';
 import './App.css'; 
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { ProductList } from './ProductList';
+import { ProductList } from '../components/ProductList';
 
 export const Home = () => {
   
